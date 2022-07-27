fd42ec42f9ef7426482feec3aac981881ae67170
Tania Paduano <al221912028@gmail.com>
2022-07-27 01:43:14 -0500

    cambio homes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,184 +8,64 @@ import {
   Image,
   Stack,
   AspectRatio,
+  ScrollView,
 } from 'native-base';
 import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import { Dimensions } from 'react-native';
 
 
 interface Props extends NativeStackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation}: Props) => {
   return (
-    <Box
-      bg={'blue.400'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      flexDir={'column'}>
-
-      <Center>
-      
-        <Image
-          source={require('../assets/images/logo1_1.jpg')}
-          alt="Alternate Text"
-          w={'64'}
-          h={'41%'}
-          zIndex={99}
-          borderRadius={20}
-        />
-        </Center>
-        <Box>
-
-        <Text isTruncated maxW="300" w="80%" bold italic underline >
-          Bienvenido Mauricio
-        </Text>
-        <Button onPress={() => navigation.replace('LoginScreen')}>
-          Cerrar Sesion
-        </Button>
-      </Box>
-
-      <Box>
-        <Text isTruncated maxW="300" w="80%" bold italic underline>
-          Has tus compras
-        </Text>
-      </Box>
-
-      <Box
-        bg={'blue.400'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        flexDir={'row'}>
-        <Box>
-          <Box
-            maxW="50%"
-            rounded="lg"
-            overflow="hidden"
-            borderColor="coolGray.200"
-            borderWidth="1"
-            _dark={{}}
-            _light={{
-              backgroundColor: 'gray.50',
-            }}>
-            <Box>
-              <AspectRatio w="100%" ratio={16 / 9}>
-                <Image
-                  source={require('../assets/images/gelatinas.jpg')}
-                  alt="Alternate Text"
-                  w={'64'}
-                  h={'50%'}
-                  zIndex={99}
-                  borderRadius={20}
-                />
-              </AspectRatio>
-              <Center
-                bg="blue.500"
-                _dark={{
-                  bg: 'violet.400',
-                }}
-                _text={{
-                  color: 'warmGray.50',
-                  fontWeight: '700',
-                  fontSize: 'xs',
-                }}
-                position="absolute"
-                bottom="0"
-                px="3"
-                py="1.5">
-                Gelatinas
-              </Center>
-            </Box>
-            <Stack p="4" space={3} bg={'blue.400'}>
-              <Text fontWeight="400">Gelatinas de sabor artificial $20</Text>
-            </Stack>
-          </Box>
+    <>
+     <Box
+      bg={'blue.100'}
+      position={'absolute'}
+      left={0}
+      top={0}
+      w={Dimensions.get('window').width}
+      h={Dimensions.get('window').height}>     
+      <Image source={require('../assets/images/logo1_1.jpg')} h={'30%'}/>
+      <Heading ml={2} mt={4} color={'black'}>
+          Has tus compras ->  
+      </Heading>
+        <ScrollView horizontal={true} mt={'6'} ml={'4'} mr={'4'}>
+           <Box w={'40'} h={'56'} bg={'blue.200'} flexDirection={'column'} shadow={'9'} mx={2}>
+            <Box borderWidth={2} h={'70%'} borderColor={'blue.400'}>
+        <Image 
+        source={require('../assets/images/gelatinas.jpg')}
+        resizeMode={'stretch'}
+        h={'100%'}
+        />        
         </Box>
-        <Box>
-          <Box
-            w={'70%'}
-            h={'50%'}
-            justifyContent={'center'}
-            rounded="lg"
-            overflow="hidden"
-            borderColor="coolGray.200"
-            borderWidth="1"
-            _dark={{}}
-            _light={{
-              backgroundColor: 'gray.50',
-            }}>
-            <Box>
-              <AspectRatio w="100%" ratio={16 / 9}>
-                <Image
-                  source={require('../assets/images/tamborcito.jpg')}
-                  alt="Alternate Text"
-                  w={'64'}
-                  h={'50%'}
-                  zIndex={99}
-                  borderRadius={20}
-                />
-              </AspectRatio>
-              <Center
-                bg="blue.500"
-                _text={{
-                  color: 'warmGray.50',
-                  fontWeight: '700',
-                  fontSize: 'xs',
-                }}
-                position="absolute"
-                bottom="0"
-                px="3"
-                py="1.5">
-                Tamarindos
-              </Center>
-            </Box>
-            <Stack p="4" space={3} bg={'blue.400'}>
-              <Text fontWeight="400">Tamarindos de sabor artificial $20</Text>
-            </Stack>
-          </Box>
+        <Box h={'30%'} alignItems={'center'}>
+          <Text h={'50%'} color={'black'} fontSize={'md'}>
+              Nombre del producto
+          </Text>
+          <Text h={'50%'} bold fontSize={'lg'}>
+              $50
+          </Text>
         </Box>
-        ;
-      </Box>
-
-      <Box>
-        <Box
-          maxW="50%"
-          rounded="lg"
-          overflow="hidden"
-          borderColor="coolGray.200"
-          borderWidth="1"
-          _dark={{}}
-          _light={{
-            backgroundColor: 'gray.50',
-          }}>
-          <Box>
-            <AspectRatio w="100%" ratio={16 / 9}>
-              <Image
-                source={require('../assets/images/gelatinas.jpg')}
-                alt="Alternate Text"
-                w={'64'}
-                h={'50%'}
-                zIndex={99}
-                borderRadius={20}
-              />
-            </AspectRatio>
-            <Center
-              bg="blue.500"
-              _text={{
-                color: 'warmGray.50',
-                fontWeight: '700',
-                fontSize: 'xs',
-              }}
-              position="absolute"
-              bottom="0"
-              px="3"
-              py="1.5">
-              Tamarindos
-            </Center>
-          </Box>
-          <Stack p="4" space={3} bg={'blue.400'}>
-            <Text fontWeight="400">Tamarindos de sabor artificial $20</Text>
-          </Stack>
+       </Box>
+        </ScrollView>
+      <Heading ml={2} color={'black'} textAlign={'center'}>
+           Nuevos productos  
+      </Heading>
+        <ScrollView horizontal={true} mt={'1'} ml={'4'} mr={'4'}>
+           <Box w={'40'} h={'40'} bg={'blue.200'} flexDirection={'column'} shadow={'9'} mx={2}>
+            <Box borderWidth={2} h={'100%'} borderColor={'blue.700'}>
+        <Image 
+        source={require('../assets/images/gelatinas.jpg')}
+        resizeMode={'stretch'}
+        h={'100%'}
+        alt='gelatinas'
+        />        
         </Box>
       </Box>
+    </ScrollView>
     </Box>
+   </>
   );
 };
